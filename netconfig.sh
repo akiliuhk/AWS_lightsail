891f3153963a013379602c5e3ffb946c2ebb63d1
akiliuse <aki.liu@suse.com>
2021-08-11 18:15:40 +0800

    Update netconfig.sh

--- netconfig.sh
+++ netconfig.sh
@@ -7,3 +7,10 @@ echo 'NETCONFIG_DNS_POLICY="STATIC"' >> /etc/sysconfig/network/config;
 echo 'NETCONFIG_DNS_STATIC_SEARCHLIST="example.com"' >> /etc/sysconfig/network/config;
 echo 'NETCONFIG_DNS_STATIC_SERVERS="18.142.61.31 172.26.0.2"' >> /etc/sysconfig/network/config;
 netconfig update -f;
+
+
+curl -s http://169.254.169.254/latest/meta-data
+
+curl -s http://169.254.169.254/latest/user-data
+
+curl -s http://169.254.169.254/latest/meta-data/public-ipv4
