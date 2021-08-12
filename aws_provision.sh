4d3ce314d45bbc913750c4daa39da3e7e3f89de4
akiliuse <aki.liu@suse.com>
2021-08-12 12:37:13 +0800

    README

--- aws_provision.sh
+++ aws_provision.sh
@@ -5,6 +5,9 @@ export STD=$1
 ### create AWS Lightsail VM
 aws lightsail create-instances --region ap-southeast-1 --instance-names  {$STD-rancher,$STD-rke-m1,$STD-rke-w1,$STD-rke-w2,$STD-rke-w3} --availability-zone ap-southeast-1a --blueprint-id opensuse_15_2 --bundle-id medium_2_0 --ip-address-type ipv4 --user-data "systemctl enable docker;systemctl start docker;" --tags key=$STD --no-cli-pager
 
+### sleep 60s to wait AWS Lightsail VM finished provision.
+sleep 60;
+
 ### open ports for AWS Lightsail VM
 aws lightsail put-instance-public-ports \
 --port-infos \
