c2fc5700ab5cda3576cab462a9b29d0be80d0e6c
akiliuse <aki.liu@suse.com>
2021-08-19 11:03:18 +0800

    Update aws_provision.sh

--- aws_provision.sh
+++ aws_provision.sh
@@ -12,8 +12,7 @@ put-instance-ports $tags-rke-m1
 put-instance-ports $tags-rke-w1
 put-instance-ports $tags-rke-w2
 put-instance-ports $tags-rke-w3
-#download-key
-get-instances
+download-key $tags
 ssh-file $tags $tags-rancher
 ssh-file $tags $tags-rke-m1
 ssh-file $tags $tags-rke-w1
@@ -35,7 +34,7 @@ aws lightsail create-instances \
     --instance-names {"$VMname1","$VMname2","$VMname3","$VMname4","$VMname5"} \
     --availability-zone ap-southeast-1a \
     --blueprint-id opensuse_15_2 \
-    --bundle-id nano_2_0 \
+    --bundle-id medium_2_0 \
     --ip-address-type ipv4 \
     --user-data "systemctl enable docker;systemctl start docker;" \
     --tags key=$tags --no-cli-pager | grep status
@@ -46,17 +45,17 @@ function check-instance-state(){
 local $tags=$1
 mkdir -p ~/$1-lab-info/
 sleep 10
-get-instances > ~/$1-lab-info/$1-get-instances.txt
+get-instances $tags
 while :
 do
-  if grep -q pending ~/$1-lab-info//$1-get-instances.txt
+  if grep -q pending ~/$1-lab-info/$1-get-instances.txt
   then
     echo 'pending VM provisioning...'
-    get-instances > ~/$1-lab-info/$1-get-instances.txt
+    get-instances $tags
     sleep 5
   else
     echo 'all VM is up and running'
-    get-instances > ~/$1-lab-info/$1-get-instances.txt
+    get-instances $tags
     break
   fi
 done
@@ -79,24 +78,25 @@ aws lightsail put-instance-public-ports \
 ### download default-key-pair
 
 function download-key(){
-aws lightsail download-default-key-pair --output text --query publicKeyBase64 > ~/$1-lab-info//lightsail-default-key.pub
+aws lightsail download-default-key-pair --output text --query publicKeyBase64 > ~/$1-lab-info/lightsail-default-key.pub
 chmod 644 ~/$1-lab-info/lightsail-default-key.pub
-aws lightsail download-default-key-pair --output text --query privateKeyBase64 > ~/$1-lab-info//lightsail-default-key.pem
-chmod 600 ~/$1-lab-info//lightsail-default-key.pem
+aws lightsail download-default-key-pair --output text --query privateKeyBase64 > ~/$1-lab-info/lightsail-default-key.pem
+chmod 600 ~/$1-lab-info/lightsail-default-key.pem
 }
 
 ### get AWS Lightsail instance
 function get-instances(){
 aws lightsail get-instances --region ap-southeast-1 \
     --query 'instances[].{publicIpAddress:publicIpAddress,privateIpAddress:privateIpAddress,VMname:name,state:state.name}' \
-    --output table --no-cli-pager
+    --output table --no-cli-pager > ~/$1-lab-info/$1-get-instances.txt
 }
 
 ### ssh command into file
 function ssh-file(){
+local tags=$1
 local ip=`aws lightsail get-instance --instance-name $2 --query instance.publicIpAddress --no-cli-pager`
-    echo 'ssh -i ~/$1-lab-info//lightsail-default-key.pem -o StrictHostKeyChecking=no ec2-user@'$ip > ~/$1-lab-info/ssh-$2.sh
-    chmod 755 ~/$1-lab-info/ssh-$2.sh                   
+    echo "ssh -i ~/$tags-lab-info/lightsail-default-key.pem -o StrictHostKeyChecking=no ec2-user@"$ip > ~/$tags-lab-info/ssh-$2.sh
+    chmod 755 ~/$tags-lab-info/ssh-$2.sh                   
 }
 
 main $1
