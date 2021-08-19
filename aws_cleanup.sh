c50cfd94f7769acc44ef40ceaa5b7dd62cff2e18
akiliuse <aki.liu@suse.com>
2021-08-19 16:06:49 +0800

    Update aws_cleanup.sh

--- aws_cleanup.sh
+++ aws_cleanup.sh
@@ -5,8 +5,9 @@ function delete-instance(){
 
 local tags=$1
 rm -f ~/$tags-lab-info/*
-rmdir ~/$tags-lab-info
 rm -f ~/$tags-lab-info.tar.gz
+rmdir ~/$tags-lab-info
+
 aws lightsail delete-key-pair --key-pair-name $tags-default-key --output text --no-cli-pager
 
 aws lightsail delete-instance --region ap-southeast-1  --instance-name $tags-rancher --output text --no-cli-pager
