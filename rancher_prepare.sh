f36d6b0b187aecc9d2427ecd2ebf574794484d1c
akiliuse <aki.liu@suse.com>
2021-09-15 15:41:59 +0800

    Update rancher_prepare.sh

--- rancher_prepare.sh
+++ rancher_prepare.sh
@@ -3,7 +3,9 @@
 function install_rancher() {
   local tags=$1
   cd ~/$tags-lab-info
-  ip=`cat $tags-get-instances.txt | grep $tags-rancher | cut -d '|' -f 4 | xargs`
+  
+  ip=`cat $tags-get-instances.txt | grep $tags-rancher | cut -d '|' -f 3 | xargs`
+
   ssh -i $tags-default-key.pem -o StrictHostKeyChecking=no ec2-user@$ip 'sudo docker run -d --restart=unless-stopped -p 80:80 -p 443:443 --privileged rancher/rancher:latest'
   container=`ssh -i $tags-default-key.pem -o StrictHostKeyChecking=no ec2-user@$ip 'sudo docker ps -q'`
   sleep 180
