03d12ea7b9fce11b8df81a8c9ea77d076f547502
akiliuse <aki.liu@suse.com>
2021-10-15 11:53:19 +0800

    Update rancher_prepare.sh

--- rancher_prepare.sh
+++ rancher_prepare.sh
@@ -5,9 +5,9 @@ function install_rancher() {
   local ip=`aws lightsail get-instance --instance-name $tags-rancher --query 'instance.publicIpAddress' --output text --no-cli-pager`
   cd ~/$tags-lab-info
   ssh -i $tags-default-key.pem -o StrictHostKeyChecking=no ec2-user@$ip 'sudo docker run -d --restart=unless-stopped -p 80:80 -p 443:443 --privileged rancher/rancher:latest'
-  
+  container=`ssh -i $tags-default-key.pem -o StrictHostKeyChecking=no ec2-user@$ip 'sudo docker ps -q'`
   sleep 60
-  ssh -i $tags-default-key.pem -o StrictHostKeyChecking=no ec2-user@$ip "sudo docker logs $(sudo docker ps -q) 2>&1 | grep Password:"
+  ssh -i $tags-default-key.pem -o StrictHostKeyChecking=no ec2-user@$ip "sudo docker logs $container 2>&1 | grep Password"
 }
 
 
